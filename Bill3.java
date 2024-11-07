c5b763a79005ffb23fb63d86c5590d36c595230c
liatbenshabatruni <liat.benshabat@post.runi.ac.il>
2024-11-07 18:59:57 +0200

    solution of number 2

--- Bill3.java
+++ Bill3.java
@@ -1,16 +1,19 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
+		// here are the inputes - three diners and thier bill
 	    String name1 = args[0];
         String name2 = args[1];
         String name3 = args[2];
 		String sum = args[3];
 		int Finalsum = Integer.parseInt(sum);
-	    //now calculate how much for each diner:
-		double a = Finalsum;
+	    //now we will use 2 vars to calculate how much for each diner:
+		//casting int to double
+		double a = Finalsum; 
 		double b;
+		//making sure it will round up
 		b = Math.ceil(a/3);
+		//printing the final price for each diner
 		System.out.print("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay ");
 		System.out.print(b);
 		System.out.print(" Shekels each.");
--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,19 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		//our inputes from command line:
+		int CurrentValue = Integer.parseInt(args[0]);
+		double Rate = Double.parseDouble(args[1]);
+		int Years = Integer.parseInt(args[2]);
+		double DivRate = Rate/100;
+		double FutureValue = CurrentValue * (Math.pow(1+DivRate, Years));
+		System.out.print("After "); 
+		System.out.print(Years); 
+		System.out.print(" years, $"); 
+		System.out.print(CurrentValue); 
+		System.out.print(" saved at "); 
+		System.out.print(Rate); 
+		System.out.print("% will yield $"); 
+		System.out.print((int) FutureValue);
 	}
 }
